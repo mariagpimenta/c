502be5e9effdd2d66f851e0ca52215277d48cf8d
mariagpimenta <147571175+mariagpimenta@users.noreply.github.com>
2024-10-07 21:12:00 -0300

    Update velha.c

--- velha.c
+++ velha.c
@@ -1,118 +1,105 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 char matrix_game[3][3] = {
-        {' ', ' ', ' '},
-        {' ', ' ', ' '},
-        {' ', ' ', ' '}
+    {' ', ' ', ' '},
+    {' ', ' ', ' '},
+    {' ', ' ', ' '}
 };
 
-
 int verifyChar(char letter) {
-
-    if (letter == 'X' || letter == 'O')
-        return 1;
-    
-    else 
-        return 0;
+    return (letter == 'X' || letter == 'O') ? 1 : 0;
 }
 
-
-void matrix() {
-
+void printMatrix() {
+    printf("\n");
     for (int i = 0; i < 3; i++) {
-
         for (int j = 0; j < 3; j++) {
-
-            printf("%i", matrix_game[i][j]);
+            printf("| %c ", matrix_game[i][j]);
         }
+        printf("|\n");
     }
+    printf("\n");
 }
-
 
 int winLine() {
-
-    int equal = 1;
-
     for (int i = 0; i < 3; i++) {
-
-        for (int j = 0; j < 3; j++) {
-
-            if (verifyChar(matrix_game[i][j]) && matrix_game[i][j] == matrix_game[i][j + 1])
-                equal++;
-            
-            if (equal == 3)
-                return 1;
-
-            equal = 1;
+        if (verifyChar(matrix_game[i][0]) && 
+            matrix_game[i][0] == matrix_game[i][1] && 
+            matrix_game[i][1] == matrix_game[i][2]) {
+            return 1; 
         }
-
-    return 0;
     }
+    return 0;
 }
-
 
 int winColumns() {
-
-    int equal = 1;
-
     for (int i = 0; i < 3; i++) {
-
-        for (int j = 0; j < 3; j++) {
-
-            if (verifyChar(matrix_game[i][j]) && matrix_game[i][j] == matrix_game[j + 1][i])
-                equal++;
-            
-            if (equal == 3)
-                return 1;
-
+        if (verifyChar(matrix_game[0][i]) && 
+            matrix_game[0][i] == matrix_game[1][i] && 
+            matrix_game[1][i] == matrix_game[2][i]) {
+            return 1; 
         }
-
-    return 0;
     }
+    return 0;
 }
-
 
 int winMainDiagonal() {
-
-    int equal = 1;
-
-    for (int i = 0; i < 2; i++) {
-
-        if (verifyChar(matrix_game[i][i]) && matrix_game[i][i] == matrix_game[i + 1][i + 1])
-            equal++;
-            
-        if (equal == 3)
-            return 1;
-
-        }
-
+    if (verifyChar(matrix_game[0][0]) && 
+        matrix_game[0][0] == matrix_game[1][1] && 
+        matrix_game[1][1] == matrix_game[2][2]) {
+        return 1; 
+    }
     return 0;
 }
-  
 
 int winSecondDiagonal() {
-
-    int equal = 1;  
-
-    for (int i = 0; i < 3; i++) {
-
-        if (verifyChar(matrix_game[i][3 - i - 1]) && matrix_game[i][3 - i - 1] == matrix_game[i + 1][3 - i - 2])
-            equal++;
-            
-        if (equal == 3)
-            return 1;
-
-        }
-
+    if (verifyChar(matrix_game[0][2]) && 
+        matrix_game[0][2] == matrix_game[1][1] && 
+        matrix_game[1][1] == matrix_game[2][0]) {
+        return 1; 
+    }
     return 0;
+}
+
+int checkWin() {
+    return winLine() || winColumns() || winMainDiagonal() || winSecondDiagonal();
 }
 
 int main() {
+    int player = 1; 
+    int row, col;
+    int moves = 0;
 
+    while (1) {
+        printMatrix();
 
+        char currentPlayer = (player % 2 == 1) ? 'X' : 'O';
+        printf("Jogador %d (%c), insira linha (0-2) e coluna (0-2): ", player, currentPlayer);
+        scanf("%d %d", &row, &col);
 
+        if (row < 0 || row > 2 || col < 0 || col > 2 || matrix_game[row][col] != ' ') {
+            printf("Movimento inválido! Tente novamente.\n");
+            continue;
+        }
+
+        matrix_game[row][col] = currentPlayer;
+        moves++;
+
+        if (checkWin()) {
+            printMatrix();
+            printf("Jogador %d (%c) venceu!\n", player, currentPlayer);
+            break;
+        }
+
+        if (moves == 9) {
+            printMatrix();
+            printf("Empate!\n");
+            break;
+        }
+
+        player++;
+    }
 
     return 0;
 }
